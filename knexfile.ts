f6db6ae3a32bb92564d140a4848e0f596bd99db6
stephen <dinhvanchuong2k@gmail.com>
2024-02-02 16:39:01 +0700

    update knex file

--- knexfile.ts
+++ knexfile.ts
@@ -1,4 +1,6 @@
 import type { Knex } from "knex";
+import dotenv from "dotenv";
+dotenv.config();
 
 // Update with your config settings.
 
@@ -6,6 +8,7 @@ const config: { [key: string]: Knex.Config } = {
   development: {
     client: 'mysql2',
     connection: {
+      host: process.env.MYSQL_HOST,
       database: process.env.MYSQL_DBNAME,
       user: process.env.MYSQL_USER,
       password: process.env.MYSQL_PWD

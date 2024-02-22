6a46161361dbdde0cfab5e03c762781862d9bc58
stephen <dinhvanchuong2k@gmail.com>
2024-02-22 16:28:46 +0700

    update script file

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -2,5 +2,5 @@
 
 echo 'run application_start.sh: ' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
 
-echo 'pm2 restart nodejs-express-app' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
-pm2 restart nodejs-express-app >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+echo 'pm2 restart app' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+pm2 restart app >> /home/ubuntu/backend/nodejs-depoyment/deploy.log

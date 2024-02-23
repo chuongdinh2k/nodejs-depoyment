62fb4fe3ba81599d87f1f8e600a38b8d45780f16
stephen <dinhvanchuong2k@gmail.com>
2024-02-23 18:01:52 +0700

    update script file

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,8 +1,12 @@
 #!/bin/bash
-echo 'run after_install.sh: ' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
 
-echo 'cd /home/ec2-user/nodejs-server-cicd' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
-cd /home/ubuntu/backend/nodejs-depoyment >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+# Redirect all outputs to the deploy.log file
+exec >> /home/ubuntu/backend/nodejs-deployment/deploy.log 2>&1
 
-echo 'npm install' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log 
-npm install >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+echo 'run after_install.sh'
+
+echo 'cd /home/ubuntu/backend/nodejs-deployment'
+cd /home/ubuntu/backend/nodejs-deployment
+
+echo 'npm install'
+npm install

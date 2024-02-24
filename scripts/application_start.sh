26a3233d109b15cc46cf9ed3c8e8de95bad547ad
stephen <dinhvanchuong2k@gmail.com>
2024-02-24 11:28:34 +0700

    update script application_start.sh

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -1,6 +1,18 @@
 #!/bin/bash
 
-echo 'run application_start.sh: ' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+# Redirect all outputs to the start.log file
+exec >> /home/ubuntu/backend/nodejs-depoyment/start.log 2>&1
 
-echo 'pm2 restart app' >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
-pm2 restart app >> /home/ubuntu/backend/nodejs-depoyment/deploy.log
+echo 'Running application_start.sh'
+
+# Change to the project directory
+PROJECT_DIR="/home/ubuntu/backend/nodejs-depoyment"
+cd "$PROJECT_DIR" || { echo "Unable to change to directory $PROJECT_DIR"; exit 1; }
+
+# Specify the full path to the pm2 executable
+PM2_PATH=$(which pm2)
+
+# Use pm2 to start your application
+$PM2_PATH start app
+
+echo 'Application start completed'

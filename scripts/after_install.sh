3cfa6e87600c10f96a5a1cc3f658ab24febbdaa4
stephen <dinhvanchuong2k@gmail.com>
2024-02-24 11:04:15 +0700

    update script

--- scripts/after_install.sh
+++ scripts/after_install.sh
@@ -1,12 +1,35 @@
 #!/bin/bash
 
+# Specify the full path to the node binary
+NODE_PATH=$(which node)
+
+# Specify the full path to the npm binary
+NPM_PATH=$(which npm)
+
+# Specify the full path to the deploy.log file
+DEPLOY_LOG_PATH="/home/ubuntu/backend/nodejs-deployment/deploy.log"
+
+# Create the deploy.log file if it doesn't exist
+touch "$DEPLOY_LOG_PATH"
+
 # Redirect all outputs to the deploy.log file
-exec >> /home/ubuntu/backend/nodejs-deployment/deploy.log 2>&1
+exec >> "$DEPLOY_LOG_PATH" 2>&1
 
 echo 'run after_install.sh'
 
-echo 'cd /home/ubuntu/backend/nodejs-deployment'
-cd /home/ubuntu/backend/nodejs-deployment
+# Specify the full path to the project directory
+PROJECT_DIR="/home/ubuntu/backend/nodejs-deployment"
 
-echo 'npm install'
-/home/ubuntu/.nvm/versions/node/v20.11.1/bin/npm install
+echo "cd $PROJECT_DIR"
+cd "$PROJECT_DIR" || exit 1
+
+echo "Installing dependencies using npm"
+"$NPM_PATH" install
+
+# Additional commands if needed
+# ...
+
+# Restart your Node.js application or perform other necessary tasks
+# ...
+
+echo "Deployment completed successfully"

0ca5d71583f9e4a1d15c3b9b6ea13f4d4c0bc49f
stephen <dinhvanchuong2k@gmail.com>
2024-02-24 11:53:12 +0700

    test commit 3

--- scripts/application_start.sh
+++ scripts/application_start.sh
@@ -5,6 +5,9 @@ exec >> /home/ubuntu/backend/nodejs-depoyment/start.log 2>&1
 
 echo 'Running application_start.sh'
 
+# Specify the full path to the npm binary
+NPM_PATH=$(which npm)
+
 # Change to the project directory
 PROJECT_DIR="/home/ubuntu/backend/nodejs-depoyment"
 cd "$PROJECT_DIR" || { echo "Unable to change to directory $PROJECT_DIR"; exit 1; }
@@ -12,12 +15,11 @@ cd "$PROJECT_DIR" || { echo "Unable to change to directory $PROJECT_DIR"; exit 1
 # Specify the full path to the pm2 executable
 PM2_PATH=$(which pm2)
 
+"$NPM_PATH" run build
+
 # Use pm2 to start your application
 "$PM2_PATH" start dist/src/app.js -f
 
-# Specify the full path to the npm binary
-NPM_PATH=$(which npm)
 
-"$NPM_PATH" run dev
 
 echo 'Application start completed'
